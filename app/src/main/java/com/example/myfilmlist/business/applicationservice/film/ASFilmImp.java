b2c6705c879665196e7ebbccce0ac4b1d232ec0e
AlexCabezas2018 <alexcabezas0705@gmail.com>
2019-04-07 21:51:05 +0200

    Eliminada clase Utils (el algoritmo ya lo hacia String.replace())

--- app/src/main/java/com/example/myfilmlist/business/applicationservice/film/ASFilmImp.java
+++ app/src/main/java/com/example/myfilmlist/business/applicationservice/film/ASFilmImp.java
@@ -1,6 +1,5 @@
 package com.example.myfilmlist.business.applicationservice.film;
 
-import com.example.myfilmlist.business.Utils;
 import com.example.myfilmlist.exceptions.ASException;
 import com.example.myfilmlist.exceptions.DAOException;
 import com.example.myfilmlist.integration.daofilm.DAOFilm;
@@ -19,7 +18,7 @@ public class ASFilmImp extends ASFilm {
     public List<TFilm> searchByName(String filmName) throws ASException {
         List<TFilm> filmsToReturn;
         try{
-            String resultFilmName = Utils.getCompoundName(filmName); //We obtain the compound name (If the tittle is composed of more than one word)
+            String resultFilmName = filmName.replace(" ", "+"); //We obtain the compound name (If the tittle is composed of more than one word)
             filmsToReturn = DAOFilm.getInstance().getFilmsByName(resultFilmName); //We call the data layer and obtain the films
             if(filmsToReturn == null) throw new ASException("The film doesn't exist"); //That means the film does not exist
         }
