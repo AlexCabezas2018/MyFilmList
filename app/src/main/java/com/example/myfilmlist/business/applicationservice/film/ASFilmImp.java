5f5a7e487731b44ce0b87aa518875770a75498b7
AlexCabezas2018 <alexcabezas0705@gmail.com>
2019-04-08 17:09:56 +0200

    Solucionado issue #1

--- app/src/main/java/com/example/myfilmlist/business/applicationservice/film/ASFilmImp.java
+++ app/src/main/java/com/example/myfilmlist/business/applicationservice/film/ASFilmImp.java
@@ -4,6 +4,8 @@ import com.example.myfilmlist.exceptions.ASException;
 import com.example.myfilmlist.exceptions.DAOException;
 import com.example.myfilmlist.integration.daofilm.DAOFilm;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
 public class ASFilmImp extends ASFilm {
@@ -18,11 +20,11 @@ public class ASFilmImp extends ASFilm {
     public List<TFilm> searchByName(String filmName) throws ASException {
         List<TFilm> filmsToReturn;
         try{
-            String resultFilmName = filmName.replace(" ", "+"); //We obtain the compound name (If the tittle is composed of more than one word)
+            String resultFilmName = URLEncoder.encode(filmName, "UTF-8"); //We encode our data in UTF-8 (because the api needs the encoded filmName)
             filmsToReturn = DAOFilm.getInstance().getFilmsByName(resultFilmName); //We call the data layer and obtain the films
             if(filmsToReturn == null) throw new ASException("The film doesn't exist"); //That means the film does not exist
         }
-        catch(DAOException | ASException exception) {
+        catch(DAOException | ASException | UnsupportedEncodingException exception) {
             throw new ASException(exception.getMessage());
         }
         return filmsToReturn;
