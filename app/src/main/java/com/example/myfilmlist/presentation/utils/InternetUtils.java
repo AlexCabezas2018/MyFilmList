7d688ff57de3c7ddd26b709a5a752cb6b0234076
AlexCabezas2018 <alexcabezas0705@gmail.com>
2019-04-12 13:26:14 +0200

    Fixes #3 Ahora alerta al usuario si no hay conexion. Tambien añadido un progressdialog por si el internet es malo y tarda en buscar, que salga un mensajito de "buscando..."

---
+++ app/src/main/java/com/example/myfilmlist/presentation/utils/InternetUtils.java
@@ -0,0 +1,20 @@
+package com.example.myfilmlist.presentation.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class InternetUtils {
+
+    /**
+     * Checks if there is internet connection
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+}
--- app/src/main/java/com/example/myfilmlist/presentation/views/activities/SearchActivity.java
+++ app/src/main/java/com/example/myfilmlist/presentation/views/activities/SearchActivity.java
@@ -1,9 +1,9 @@
 package com.example.myfilmlist.presentation.views.activities;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
@@ -12,33 +12,42 @@ import com.example.myfilmlist.R;
 import com.example.myfilmlist.business.film.TFilmPreview;
 import com.example.myfilmlist.exceptions.ASException;
 import com.example.myfilmlist.integration.utils.PreviewListAdapter;
+import com.example.myfilmlist.presentation.utils.InternetUtils;
 import com.example.myfilmlist.presentation.context.Context;
 import com.example.myfilmlist.presentation.events.Events;
 import com.example.myfilmlist.presentation.presenter.Presenter;
 import com.example.myfilmlist.presentation.views.UpdatingView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SearchActivity extends AppCompatActivity implements UpdatingView {
 
     private SearchView mSearchView;
+    ProgressDialog progressDialog;
     private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Searching...");
         mListView = findViewById(R.id.listView);
         mSearchView = findViewById(R.id.titleSearch);
         mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 try {
-                    String data = mSearchView.getQuery().toString();
-                    Presenter.getInstance().action(new Context(Events.SEARCH_BY_NAME, SearchActivity.this, data));
+                    if(InternetUtils.isNetworkAvailable(getApplicationContext())) {
+                        progressDialog.show();
+                        String data = mSearchView.getQuery().toString();
+                        Presenter.getInstance().action(new Context(Events.SEARCH_BY_NAME, SearchActivity.this, data));
+                    }
+                    else throw new ASException("There was a problem while trying to connect to internet");
                 }
                 catch (ASException ex){
+                    progressDialog.dismiss();
                     ex.showMessage(SearchActivity.this);
                     return false;
                 }
@@ -58,6 +67,7 @@ public class SearchActivity extends AppCompatActivity implements UpdatingView {
 
     @Override
     public void update(Context resultData) {
+        progressDialog.dismiss();
         PreviewListAdapter adapter = new PreviewListAdapter(this, R.layout.film_preview_layout, (List<TFilmPreview>) resultData.getData() );
         mListView.setAdapter(adapter);
     }
